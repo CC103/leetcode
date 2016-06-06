75ec76fca0a2035ba413ab9688aefa0a4915d2fc
Chuck Liu <yimingl816@gmail.com>
2016-06-06 15:27:17 +0800

    Create 118. Pascal's Triangle.cpp

---
+++ 118. Pascal's Triangle.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+class Solution {
+public:
+	vector<vector<int>> generate(int numRows) {
+		vector<vector<int>> triangle;
+		if (numRows == 0)
+			return triangle;
+		for (int i = 0; i < numRows; i++){
+			vector<int> row;
+			triangle.push_back(row);
+		}
+		triangle[0].push_back(1);
+		for (int i = 1; i < numRows; i++){
+			triangle[i].push_back(1);
+			for (int j = 1; j < i; j++){
+				triangle[i].push_back(triangle[i - 1][j - 1] + triangle[i - 1][j]);
+			}
+			triangle[i].push_back(1);
+		}
+		return triangle;
+	}
+};
