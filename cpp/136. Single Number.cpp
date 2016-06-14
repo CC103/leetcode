857f6af3ef989b344d718053e49c5c100b5edd55
Chuck Liu <yimingl816@gmail.com>
2016-06-14 14:35:56 +0800

    Create 136. Single Number.cpp

---
+++ cpp/136. Single Number.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+#include<string>
+#include<vector>
+using namespace std;
+//数据取值范围是（-20000, 20000）
+class Solution {
+public:
+	int singleNumber(vector<int>& nums) {
+		int hash[40000] = { 0 };
+		for (int i = 0; i < nums.size(); i++){
+			if (nums[i] < 0)
+				hash[nums[i] * -1 + 20000]++;
+			else
+				hash[nums[i]]++;
+		}
+		for (int i = 0; i < 40000; i++){
+			if (hash[i] == 1){
+				if (i < 20000)
+					return i;
+				else
+					return (i - 20000) * -1;
+			}
+
+		}
+		return 0;
+	}
+};
