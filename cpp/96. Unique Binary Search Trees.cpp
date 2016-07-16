b8065e6ecbaa66b7048d3a40b02848153b704c19
Chuck Liu <yimingl816@gmail.com>
2016-07-16 15:23:20 +0800

    Create 96. Unique Binary Search Trees.cpp

---
+++ cpp/96. Unique Binary Search Trees.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+class Solution {
+public:
+    int numTrees(int n) {
+        int *G = new int[n + 1];
+        G[0] = 1;
+        G[1] = 1;
+        for(int i = 2; i <= n; i++){
+            //the value of G[i] now is unpredictable
+            G[i] = 0;
+            for(int j = 1; j <= i; j++){
+                G[i] += G[j - 1] * G[i - j];
+            }
+        }
+        return G[n];
+    }
+
+};
