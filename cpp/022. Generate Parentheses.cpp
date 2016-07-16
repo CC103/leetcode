615d8f04934baa8a71a4d88d2064cb237afad29e
Chuck Liu <yimingl816@gmail.com>
2016-07-16 11:21:12 +0800

    Update 022. Generate Parentheses.cpp

--- cpp/022. Generate Parentheses.cpp
+++ cpp/022. Generate Parentheses.cpp
@@ -1,1 +1,23 @@
-
+#include <iostream>
+#include <vector>
+#include <string>
+using namespace std;
+class Solution {
+public:
+    vector<string> generateParenthesis(int n) {
+        vector<string> v;
+        string str = "";
+        s(v, str, n, 0);
+        return v;
+    }
+    void s(vector<string> &v, string str, int n, int m){
+        if(n == 0 && m == 0){
+            v.push_back(str);
+            return;
+        }
+        if(n > 0)
+            s(v, str + "(", n - 1, m + 1);
+        if(m > 0)
+            s(v, str + ")", n, m - 1);
+    }
+};
