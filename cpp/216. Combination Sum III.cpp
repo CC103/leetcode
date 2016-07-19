53a1ef882678bf76f7cdb35557cbccf44eaf71b3
Chuck Liu <yimingl816@gmail.com>
2016-07-19 17:37:59 +0800

    Update 216. Combination Sum III.cpp

--- cpp/216. Combination Sum III.cpp
+++ cpp/216. Combination Sum III.cpp
@@ -1,1 +1,26 @@
+class Solution {
+public:
+    vector<vector<int>> combinationSum3(int k, int n) {
+        vector<vector<int>> res;
+        vector<int> sol;
+        recursiveCal(res, sol, k, n);
+        return res;
+    }
+    void recursiveCal(vector<vector<int>> &res, vector<int> &sol, int k, int n){
+    	if(sol.size() == k && n == 0){
+    		res.push_back(sol);
+    		return;
+    	}
+    	if(sol.size() < k){
+    	for(int i = sol.empty()? 1 : sol.back() + 1; i < 10; i++){
+    		if(i > n)
+    			break;
+    		//try each possible value here 
+    		sol.push_back(i);
+    		recursiveCal(res, sol, k, n - i);
+    		sol.pop_back();
+    	}
+    	}
 
+    }
+};
