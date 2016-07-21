5dc0235efb2cc2e104aa0b0645e737f602627afc
Chuck Liu <yimingl816@gmail.com>
2016-07-21 14:29:23 +0800

    Update 062. Unique Paths.cpp
    solution 2: Faster DP method without recursive function

--- cpp/062. Unique Paths.cpp
+++ cpp/062. Unique Paths.cpp
@@ -1,14 +1,16 @@
-//The easiest recursive dp solution with low speed
-//Can not pass the test because limited time 
+//Faaster DP solution without recursive function
+
 #include <iostream>
 
 class Solution {
 public:
     int uniquePaths(int m, int n) {
-     	  if(m <= 0 || n <= 0)
-     		  return 0;
-        if(m == 1 || n == 1)
-        	return 1;
-        return uniquePaths(m - 1, n) + uniquePaths(m, n - 1);
+    	vector<vector<int>> v(m, vector<int>(n, 1));
+    	for(int i = 1; i < m; i++){
+    		for(int j = 1; j < n; j++){
+    			v[i][j] = v[i - 1][j] + v[i][j - 1];
+    		}
+    	}
+    	return v[m - 1][n - 1];
     }
 };
