d8e6058a8777401b59788a6f712f058e468372de
Chuck Liu <yimingl816@gmail.com>
2016-07-21 14:09:51 +0800

    Create 062. Unique Paths.cpp
    solution 1: slow recursive dp method

---
+++ cpp/062. Unique Paths.cpp
@@ -0,0 +1,14 @@
+//The easiest recursive dp solution with low speed
+//Can not pass the test because limited time 
+#include <iostream>
+
+class Solution {
+public:
+    int uniquePaths(int m, int n) {
+     	  if(m <= 0 || n <= 0)
+     		  return 0;
+        if(m == 1 || n == 1)
+        	return 1;
+        return uniquePaths(m - 1, n) + uniquePaths(m, n - 1);
+    }
+};
