54f2574f2e64372d5684183124c80aa1706fd3d9
Chuck Liu <yimingl816@gmail.com>
2016-07-22 11:33:48 +0800

    Update 153. Find Minimum in Rotated Sorted Array.cpp
    Solution 2: Binary search. Pay attention to boundary conditions

--- cpp/153. Find Minimum in Rotated Sorted Array.cpp
+++ cpp/153. Find Minimum in Rotated Sorted Array.cpp
@@ -5,10 +5,20 @@ using namespace std;
 class Solution {
 public:
     int findMin(vector<int>& nums) {
-        for(int i = 1; i < nums.size(); i++){
-        	if(nums[i] < nums[i - 1])
-        		return nums[i];
-        }
-        return nums[0];
+    	int start = 0;
+    	int end = nums.size() - 1;
+    	int mid;
+   		while(1){
+   			if(nums[start] <= nums[end])
+   				return nums[start];
+   			mid = (start + end) / 2;
+   			if(nums[mid] >= nums[start]){
+   				//Increase 1 here is very important, it makes sure 'start' keeps moving
+   				start = mid + 1;
+   			}
+   			else{
+   				end = mid;
+   			}
+   		}
     }
 };
