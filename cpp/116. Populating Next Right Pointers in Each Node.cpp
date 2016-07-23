8dbbf973bdf684af5e53a8e35939ab74521cea65
Chuck Liu <yimingl816@gmail.com>
2016-07-23 15:42:59 +0800

    Update 116. Populating Next Right Pointers in Each Node.cpp
    Solution 2: Simple method using pointers

--- cpp/116. Populating Next Right Pointers in Each Node.cpp
+++ cpp/116. Populating Next Right Pointers in Each Node.cpp
@@ -3,33 +3,21 @@ public:
     void connect(TreeLinkNode *root) {
     	if(root == NULL)
     		return;
-    	vector<vector<TreeLinkNode*>> vecLevels;
-    	//Put root into first level vector
-    	vector<TreeLinkNode*> level_0;
-    	level_0.push_back(root);
-    	vecLevels.push_back(level_0);
-    	//put nodes into vector grouped by level
-    	for(int i = 0; i < vecLevels.size(); i++){
-    		vector<TreeLinkNode*> nextLevel;
-    		for(int j = 0; j < vecLevels[i].size(); j++){
-    			TreeLinkNode* p = vecLevels[i][j];
-    			//The tree is perfect, left and right exist in the same time 
-    			if(p->left){
-    				nextLevel.push_back(p->left);
-    				nextLevel.push_back(p->right);
-    			}
-    			//The tree is perfect, only leaves don't have child
-    			else
-    			    break;
+    	TreeLinkNode *pre;
+    	TreeLinkNode *cur;
+    	pre = root;
+    	//we don't need traversal leaves level 
+    	while(pre->left){
+    		cur = pre;
+    		//Make nodes in next level linked one by one
+    		while(cur){
+    			cur->left->next = cur->right;
+    			if(cur->next)
+    				cur->right->next = cur->next->left;
+    			//Nodes in this level has been linked, so we can traversal this level by 'next'
+    			cur = cur->next;
     		}
-    		if(!nextLevel.empty())
-    			vecLevels.push_back(nextLevel);
-    	}
-
-    	for(int i = 0; i < vecLevels.size(); i++){
-    		for(int j = 0; j < vecLevels[i].size() - 1; j++){
-    			vecLevels[i][j]->next = vecLevels[i][j + 1];
-    		}
+    		pre = pre->left;
     	}
     }
 };
