0b95ea34c938de064053e0b19d8b5e8a63d19658
Chuck Liu <yimingl816@gmail.com>
2016-07-23 15:15:57 +0800

    Update 116. Populating Next Right Pointers in Each Node.cpp
    Solution 1: The problem is in line 12; I set i <= vecLevels.size() as a condition, it should be i < vecLevels.size()

--- cpp/116. Populating Next Right Pointers in Each Node.cpp
+++ cpp/116. Populating Next Right Pointers in Each Node.cpp
@@ -3,32 +3,32 @@ public:
     void connect(TreeLinkNode *root) {
     	if(root == NULL)
     		return;
-    	vector<vector<TreeLinkNode*>> vecByLevel;
+    	vector<vector<TreeLinkNode*>> vecLevels;
     	//Put root into first level vector
     	vector<TreeLinkNode*> level_0;
     	level_0.push_back(root);
-    	vecByLevel.push_back(level_0);
+    	vecLevels.push_back(level_0);
     	//put nodes into vector grouped by level
-    	for(int i = 0; i <= vecByLevel.size(); i++){
+    	for(int i = 0; i < vecLevels.size(); i++){
     		vector<TreeLinkNode*> nextLevel;
-    		for(int j = 0; j < vecByLevel[i].size(); j++){
-    			TreeLinkNode* p = vecByLevel[i][j];
+    		for(int j = 0; j < vecLevels[i].size(); j++){
+    			TreeLinkNode* p = vecLevels[i][j];
     			//The tree is perfect, left and right exist in the same time 
-    			if(p->left != NULL){
+    			if(p->left){
     				nextLevel.push_back(p->left);
     				nextLevel.push_back(p->right);
     			}
     			//The tree is perfect, only leaves don't have child
     			else
-    				break;
+    			    break;
     		}
     		if(!nextLevel.empty())
-    			vecByLevel.push_back(nextLevel);
+    			vecLevels.push_back(nextLevel);
     	}
 
-    	for(int i = 0; i < vecByLevel.size(); i++){
-    		for(int j = 0; j < vecByLevel[i].size() - 1; j++){
-    			vecByLevel[i][j]->next = vecByLevel[i][j + 1];
+    	for(int i = 0; i < vecLevels.size(); i++){
+    		for(int j = 0; j < vecLevels[i].size() - 1; j++){
+    			vecLevels[i][j]->next = vecLevels[i][j + 1];
     		}
     	}
     }
