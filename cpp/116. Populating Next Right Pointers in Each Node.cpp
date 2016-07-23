af8f61c66a7a76a2c310b6de2babbb9568f6aab4
Chuck Liu <yimingl816@gmail.com>
2016-07-23 14:48:12 +0800

    Update 116. Populating Next Right Pointers in Each Node.cpp
    Solution 1: Runtime error, still finding bug

--- cpp/116. Populating Next Right Pointers in Each Node.cpp
+++ cpp/116. Populating Next Right Pointers in Each Node.cpp
@@ -1,25 +1,35 @@
 class Solution {
 public:
     void connect(TreeLinkNode *root) {
-    	vector<vector<TreeNode*>> vecByLevel;
-    	int i = 0;
-    	vector<TreeNode*> level0;
-    	level0.push_back(root);
-    	vecByLevel.push_back(level0);
+    	if(root == NULL)
+    		return;
+    	vector<vector<TreeLinkNode*>> vecByLevel;
+    	//Put root into first level vector
+    	vector<TreeLinkNode*> level_0;
+    	level_0.push_back(root);
+    	vecByLevel.push_back(level_0);
+    	//put nodes into vector grouped by level
     	for(int i = 0; i <= vecByLevel.size(); i++){
-    		vector<TreeNode*> nextLevel;
+    		vector<TreeLinkNode*> nextLevel;
     		for(int j = 0; j < vecByLevel[i].size(); j++){
-    			TreeNode* p = vecByLevel[i][j];
-    			//The tree is perfect 
+    			TreeLinkNode* p = vecByLevel[i][j];
+    			//The tree is perfect, left and right exist in the same time 
     			if(p->left != NULL){
     				nextLevel.push_back(p->left);
     				nextLevel.push_back(p->right);
     			}
-
+    			//The tree is perfect, only leaves don't have child
+    			else
+    				break;
     		}
-
+    		if(!nextLevel.empty())
+    			vecByLevel.push_back(nextLevel);
     	}
 
-        
+    	for(int i = 0; i < vecByLevel.size(); i++){
+    		for(int j = 0; j < vecByLevel[i].size() - 1; j++){
+    			vecByLevel[i][j]->next = vecByLevel[i][j + 1];
+    		}
+    	}
     }
 };
