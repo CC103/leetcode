3a4a2d66f0160d78d217b891e4828da4ac3be872
Chuck Liu <yimingl816@gmail.com>
2016-07-26 18:18:16 +0800

    Update 313. Super Ugly Number.cpp
    Solution 2: Still not understand

--- cpp/313. Super Ugly Number.cpp
+++ cpp/313. Super Ugly Number.cpp
@@ -1,39 +1,19 @@
 /*
 Core idea: 
-任意一个合数都可以被分解为质数的积且super ugly number的所有质因子都在规定集合内
-可推导出super ugly number是集合内任意数的乘积
-因此一个满足要求的super ugly number不断去除规定集合内的质数，一定可以被整除到1
+Every ugly number is constructed from multiply a previous ugly number by one of the primes in the list
 */
 class Solution {
 public:
-    int nthSuperUglyNumber(int n, vector<int>& primes) {
-        int size = primes.size();
-        int num = 0;
-        int i = 1;
-        //loop until find the nth super ugly number
-        while(num < n){
-            int tmp = i;
-            bool flag = true;
-            //loop until we can determine if i is a super ugly number
-            while(flag){
-                //keep doing division
-                for(int j = 0; j < primes.size(); j++){
-                    //if tmp == 1, it means i is product of numbers which are all in "primes"
-                    if(tmp == 1){
-                        num++;
-                        flag = 0;
-                        break;
-                    }
-                    if(tmp % primes[j] == 0){
-                        tmp /= primes[j];
-                        break;
-                    }
-                    if(j == primes.size() - 1)
-                        flag = false;
-                }
-            }
-            i++;
+int nthSuperUglyNumber(int n, vector<int>& primes) {
+        vector<int> index(primes.size(), 0);
+        vector<int> ugly(n, INT_MAX);
+        ugly[0] = 1;
+        for(int i = 1; i < n; i++){
+            for(int j = 0; j < primes.size(); j++) 
+                ugly[i] = min(ugly[i], ugly[index[j]] * primes[j]);
+            for(int j = 0; j < primes.size(); j++) 
+                index[j] += (ugly[i] == ugly[index[j]] * primes[j]);
         }
-        return i - 1;
-    }
+        return ugly[n - 1];
+}
 };
