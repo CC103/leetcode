4abab52a26306389dabf5e77b3ee5bbc28cc1826
Chuck Liu <yimingl816@gmail.com>
2016-07-31 12:39:58 +0800

    Update 215. Kth Largest Element in an Array.cpp
    Solution 2: Quick sort with inspiration of binary search; slow 200ms

--- cpp/215. Kth Largest Element in an Array.cpp
+++ cpp/215. Kth Largest Element in an Array.cpp
@@ -1,17 +1,35 @@
 class Solution {
 public:
     int findKthLargest(vector<int>& nums, int k) {
-        //insertion sort
-        for(int i = 1; i < nums.size(); i++){
-        	int key = nums[i];
-        	int j = i - 1;
-        	while(j >= 0 && nums[j] > key){
-        		nums[j + 1] = nums[j];
-        		j--;
-        	}
-        	nums[j + 1] = key;
-        }
-
-        return nums[nums.size() - k];
+    	int left = 0, right = nums.size() - 1;
+    	while(1){
+    		int pos = partition(nums, left, right);
+    		//pos + 1 = k; 
+    		if(pos == k - 1)
+    			return nums[pos];
+    		else if(pos > k - 1)
+    			right = pos - 1;
+    		else
+    			left = pos + 1;
+    	}
+    }
+    int partition(vector<int>& nums, int left, int right){
+    	int l = left + 1;
+    	int r = right;
+    	int pivot = nums[left];
+    	while(l <= r){
+    		if(nums[l] < pivot && nums[r] > pivot)
+    			swap(nums[l++], nums[r--]);
+    		if(nums[l] >= pivot)
+    			l++;
+    		if(nums[r] <= pivot)
+    			r--;
+    	}
+    	//put pivot into the right position
+    	//in the end of the loop, nums[l] <= pivot, nums[r]>= pivot
+    	//pivot is the leftmost element so swap it with nums[r]
+    	swap(nums[left], nums[r]);
+    	//pivot is the r+1 th largest elemrnt
+    	return r;
     }
 };
