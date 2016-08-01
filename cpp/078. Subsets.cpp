6463fa50fe92340a3724643e2f8c5af7ac43a294
Chuck Liu <yimingl816@gmail.com>
2016-08-01 14:08:09 +0800

    Update 078. Subsets.cpp
    Solution 2: iterative; Not very fast

--- cpp/078. Subsets.cpp
+++ cpp/078. Subsets.cpp
@@ -2,20 +2,17 @@ class Solution {
 public:
     vector<vector<int>> subsets(vector<int>& nums) {
     	sort(nums.begin(), nums.end());
-    	vector<vector<int>> sets;
-    	vector<int> set;
-    	findSubsets(sets, set, nums, 0);
+    	vector<vector<int>> sets(1, vector<int>());
+    	//iterative
+    	for(int i = 0; i < nums.size(); i++){
+    		int n = sets.size();
+    		for(int j = 0; j < n; j++){
+    			//push new number into existed subsets to make new subsets1
+    			sets.push_back(sets[j]);
+    			sets.back().push_back(nums[i]);
+    		}
+    	}
     	return sets;
         
-    }
-
-    //find all distinct subsets whose range begin with 'begin' 
-    void findSubsets(vector<vector<int>>& sets, vector<int>& set, vector<int>& nums, int begin){
-    	sets.push_back(set);
-    	for(int i = begin; i < nums.size(); i++){
-    		set.push_back(nums[i]);
-    		findSubsets(sets, set, nums, i + 1);
-    		set.pop_back();
-    	}
     }
 };
