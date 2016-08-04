51f0499ee096f6d5baf8c65c3e5980ef1a754151
Chuck Liu <yimingl816@gmail.com>
2016-08-04 14:23:08 +0800

    Update 201. Bitwise AND of Numbers Range.cpp
    Solution 2:the result of a range bitwise is the common 'left header' of m and n; Fast

--- cpp/201. Bitwise AND of Numbers Range.cpp
+++ cpp/201. Bitwise AND of Numbers Range.cpp
@@ -1,10 +1,13 @@
 class Solution {
 public:
     int rangeBitwiseAnd(int m, int n) {
-        int temp = m;
-        for(int i = m + 1; i <= n; i++){
-            temp &= i; 
+        int degree = 0;
+        while(m != n){
+        	m >>= 1;
+        	n >>= 1;
+        	degree++;
         }
-        return temp;
+        //now m equals n
+        return n << degree;
     }
 };
