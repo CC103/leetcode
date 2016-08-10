b238055fc7b23ec0d34d28d2342e23fad2c5ecda
Chuck Liu <yimingl816@gmail.com>
2016-08-10 17:38:38 +0800

    Update 313. Super Ugly Number.cpp
    Solution 1: based on the idea of 264. Ugly Number II; Not very fast

--- cpp/313. Super Ugly Number.cpp
+++ cpp/313. Super Ugly Number.cpp
@@ -4,16 +4,30 @@ Every ugly number is constructed from multiply a previous ugly number by one of 
 */
 class Solution {
 public:
-int nthSuperUglyNumber(int n, vector<int>& primes) {
-        vector<int> index(primes.size(), 0);
-        vector<int> ugly(n, INT_MAX);
-        ugly[0] = 1;
+    int nthSuperUglyNumber(int n, vector<int>& primes) {
+        if(n == 0) return 0;
+        if(n == 1) return 1;
+        vector<int> k(n);
+        k[0] = 1;
+        //save the current position of each prime 
+        int m = primes.size();
+        vector<int> cur(m, 0);
+        //save the temporary value
+        vector<int> temp(m);
         for(int i = 1; i < n; i++){
-            for(int j = 0; j < primes.size(); j++) 
-                ugly[i] = min(ugly[i], ugly[index[j]] * primes[j]);
-            for(int j = 0; j < primes.size(); j++) 
-                index[j] += (ugly[i] == ugly[index[j]] * primes[j]);
+            for(int j = 0; j < m; j++)
+                temp[j] = k[cur[j]] * primes[j];
+            int min = temp[0];
+            for(int it : temp){
+                if(it < min)
+                    min = it;
+            }
+            for(int j = 0; j < m; j++){
+                if(min == temp[j])
+                    cur[j]++;
+            }
+            k[i] = min;
         }
-        return ugly[n - 1];
-}
+        return k[n - 1];
+    }
 };
