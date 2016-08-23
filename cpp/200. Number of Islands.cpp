7dc441552fcf0d0c38a4d92c83ad80f151669c4c
Chuck Liu <yimingl816@gmail.com>
2016-08-23 14:18:37 +0800

    Update 200. Number of Islands.cpp
    Solution 1: depth-first search

--- cpp/200. Number of Islands.cpp
+++ cpp/200. Number of Islands.cpp
@@ -18,18 +18,14 @@ public:
     }
 
     void DFS(vector<vector<char>>& grid, int i, int j, int &m, int &n){
-        //grid[i][j] != '1'要放到最后，因为短路运算放在最后不会出现访问冲突
-        if(i >= m || i < 0 || j >= n || j < 0 || grid[i][j] != '1' )
-            return;
-        cout << "f"<< endl;
-        grid[i][j] == '0';
+        grid[i][j] = '0';
         if(i + 1 < m && grid[i + 1][j] == '1')
             DFS(grid, i + 1, j, m, n);
-        if(i - 1 > 0 && grid[i - 1][j] == '1')
+        if(i - 1 >= 0 && grid[i - 1][j] == '1')
             DFS(grid, i - 1, j, m, n);
         if(j + 1 < n && grid[i][j + 1] == '1')
             DFS(grid, i, j + 1, m, n);
-        if(j - 1 > 0 && grid[i][j - 1] == '1')
+        if(j - 1 >= 0 && grid[i][j - 1] == '1')
             DFS(grid, i, j - 1, m, n);
         return; 
     }
