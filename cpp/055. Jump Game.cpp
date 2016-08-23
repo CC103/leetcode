3aa60dfb185ac802032b8e58ecd8deeca8626d72
Chuck Liu <yimingl816@gmail.com>
2016-08-23 17:15:48 +0800

    Update 055. Jump Game.cpp
    Solution 1: greedy

--- cpp/055. Jump Game.cpp
+++ cpp/055. Jump Game.cpp
@@ -1,22 +1,12 @@
 class Solution {
 public:
     bool canJump(vector<int>& nums) {
-        if(nums.empty())
-            return false;
-        return jump(nums, 0);
-    }
-
-    bool jump(vector<int>& nums, int start){
-        int maxLen = nums[start];
-        if(start + maxLen >= nums.size() - 1)
-            return true;
-        //it can not be the last index now
-        if(maxLen == 0)
-            return false;
-        for(int i = maxLen; i > 0; i--){
-            if(jump(nums, start + i))
-                return true;
+        int size = nums.size();
+        int pos = 0;
+        for(int reach = 0; reach >= pos && pos < size; pos++){
+            //update the largest range we can reach
+            reach = max(pos + nums[pos], reach);
         }
-        return false;
+        return pos == size;
     }
 };
