3b799216b65cf61ee2fbfaf206abe55f1936eed9
Chuck Liu <yimingl816@gmail.com>
2016-08-24 17:16:47 +0800

    Update 082. Remove Duplicates from Sorted List II.cpp
    Solution 2: linked list; Fast

--- cpp/082. Remove Duplicates from Sorted List II.cpp
+++ cpp/082. Remove Duplicates from Sorted List II.cpp
@@ -11,28 +11,22 @@ public:
     ListNode* deleteDuplicates(ListNode* head) {
         if(head == NULL)
             return head;
-        map<int, int> hash;
-        map<int, int>::iterator it;
         ListNode dummy(0);
         dummy.next = head;
-        while(head){
-            it = hash.find(head->val);
-            if(it != hash.end())
-                it->second++;
-            else
-                hash[head->val] = 0;
-            head = head->next;
-        }
-        head = dummy.next;
         ListNode *last = &dummy;
         while(head){
-            if(hash.find(head->val)->second){
-                last->next = head->next;
+            if(head->next == NULL)
+                break;
+            if(head->next->val != head->val){
+                last = last->next;
                 head = head->next;
             }
             else{
-                last = head;
-                head = head->next;
+                int temp = head->val;
+                while(head != NULL && head->val == temp){
+                    head = head->next;
+                }
+                last->next = head;
             }
         }
         return dummy.next;
