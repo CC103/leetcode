eaee23dcc013308c048101a6f943cb58488b6b56
Chuck Liu <yimingl816@gmail.com>
2016-08-24 18:48:22 +0800

    Update 209. Minimum Size Subarray Sum.cpp
    Solution 2: same method without using vector

--- cpp/209. Minimum Size Subarray Sum.cpp
+++ cpp/209. Minimum Size Subarray Sum.cpp
@@ -3,32 +3,13 @@ public:
     int minSubArrayLen(int s, vector<int>& nums) {
         if(nums.empty())
             return 0;
-        vector<int> subArray;
-        int sum = 0;
-        int i = 0;
-        int minLen = nums.size() + 1;
-        while(i <= nums.size()){
-            if(sum > s){
-                minLen = subArray.size() < minLen ? subArray.size() : minLen;
-                sum -= subArray[0];
-                subArray.erase(subArray.begin());
-            }
-            else if(sum < s){
-                if(i == nums.size())
-                    break;
-                subArray.push_back(nums[i]);
-                sum += nums[i];
-                i++;
-            }
-            else{
-                minLen = subArray.size() < minLen ? subArray.size() : minLen;
-                if(i == nums.size())
-                    break;
-                sum -= subArray[0];
-                subArray.erase(subArray.begin());
-                subArray.push_back(nums[i]);
-                sum += nums[i];
-                i++;
+        int sum = 0, start = 0, n = nums.size();
+        int minLen = n + 1;
+        for(int i = 0; i < n; i++){
+            sum += nums[i];
+            while(sum >= s){
+                minLen = min(minLen, i - start + 1);
+                sum -= nums[start++];
             }
         }
         if(minLen == nums.size() + 1)
