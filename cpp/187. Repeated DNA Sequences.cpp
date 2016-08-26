bcbff0ebc9548676e96cc2c89cdfa800c9c0a482
Chuck Liu <yimingl816@gmail.com>
2016-08-26 15:49:14 +0800

    Update 187. Repeated DNA Sequences.cpp
    Solution 2: code optimization; A little better but still slow

--- cpp/187. Repeated DNA Sequences.cpp
+++ cpp/187. Repeated DNA Sequences.cpp
@@ -1,37 +1,31 @@
 class Solution {
 public:
     vector<string> findRepeatedDnaSequences(string s) {
-        vector<string> subStrings;
         vector<string> ret;
         map<int, int> m;
-        cout << "start";
-        for(int i = 0; i < int(s.size()) - 9; i++){
-            subStrings.push_back(s.substr(i, 10));
-        }   
-        int hash;
         map<int, int>::iterator it;
-        for(auto i : subStrings){
+        int hash;
+        for(int i = 0; i < int(s.size()) - 9; i++){
             hash = 0;
-            for(auto j : i){
-                //make sure that each char could occupy unique position 
+            string subString = s.substr(i, 10);
+            for(auto c : subString){
                 hash = hash << 2;
                 //A-0; C-1; G-2; T-3
-                if(j == 'C')
+                if(c == 'C')
                     hash += 1;
-                else if(j == 'G')
+                else if(c == 'G')
                     hash += 2;
-                else if(j == 'T')
+                else if(c == 'T')
                     hash += 3;
             }
-            cout << i << ": " << hash << endl;
             it = m.find(hash);
             if(it == m.end())
                 m[hash] = 1;
             else if(it->second == 1){
-                ret.push_back(i);
+                ret.push_back(subString);
                 it->second++;
             }
-        }
+        }   
         return ret;
     }
 };
