064059f1c8fa929682e5c8b793ef04cf5689a1e8
Chuck Liu <yimingl816@gmail.com>
2016-08-26 15:39:13 +0800

    Update 187. Repeated DNA Sequences.cpp
    Solution 2: hash table + bit manipulation; Slow code

--- cpp/187. Repeated DNA Sequences.cpp
+++ cpp/187. Repeated DNA Sequences.cpp
@@ -3,24 +3,33 @@ public:
     vector<string> findRepeatedDnaSequences(string s) {
         vector<string> subStrings;
         vector<string> ret;
-        if(s.empty())
-            return ret;
+        map<int, int> m;
+        cout << "start";
         for(int i = 0; i < int(s.size()) - 9; i++){
             subStrings.push_back(s.substr(i, 10));
         }   
-        bool isRepeated;
-        for(int i = 0; i < subStrings.size(); i++){
-            isRepeated = false;
-            if(!subStrings[i].empty()){
-                for(int j = i + 1; j < subStrings.size(); j++){
-                    if(subStrings[i] == subStrings[j]){
-                        if(!isRepeated){
-                            ret.push_back(subStrings[i]);
-                            isRepeated = true;
-                        }
-                        subStrings[j] = "";
-                    }
-                }
+        int hash;
+        map<int, int>::iterator it;
+        for(auto i : subStrings){
+            hash = 0;
+            for(auto j : i){
+                //make sure that each char could occupy unique position 
+                hash = hash << 2;
+                //A-0; C-1; G-2; T-3
+                if(j == 'C')
+                    hash += 1;
+                else if(j == 'G')
+                    hash += 2;
+                else if(j == 'T')
+                    hash += 3;
+            }
+            cout << i << ": " << hash << endl;
+            it = m.find(hash);
+            if(it == m.end())
+                m[hash] = 1;
+            else if(it->second == 1){
+                ret.push_back(i);
+                it->second++;
             }
         }
         return ret;
