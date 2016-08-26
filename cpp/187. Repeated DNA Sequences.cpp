ff1697ea5faa1331ff6f45a72b8e4e4b149ebacb
Chuck Liu <yimingl816@gmail.com>
2016-08-26 16:10:56 +0800

    Update 187. Repeated DNA Sequences.cpp
    Solution 3: using int array but map; Fast

--- cpp/187. Repeated DNA Sequences.cpp
+++ cpp/187. Repeated DNA Sequences.cpp
@@ -2,30 +2,37 @@ class Solution {
 public:
     vector<string> findRepeatedDnaSequences(string s) {
         vector<string> ret;
-        map<int, int> m;
-        map<int, int>::iterator it;
-        int hash;
-        for(int i = 0; i < int(s.size()) - 9; i++){
-            hash = 0;
-            string subString = s.substr(i, 10);
-            for(auto c : subString){
-                hash = hash << 2;
-                //A-0; C-1; G-2; T-3
-                if(c == 'C')
-                    hash += 1;
-                else if(c == 'G')
-                    hash += 2;
-                else if(c == 'T')
-                    hash += 3;
+        int hashTable[1048576] = {0};
+        int hash = 0;
+        char c;
+        if(s.size() < 11)
+            return ret;
+        //initialization 
+        for(int i = 0; i < 9; i++){
+            hash = hash << 2;
+            c = s[i];
+            if(c == 'C')
+                hash += 1;
+            else if(c == 'G')
+                hash += 2;
+            else if(c == 'T')
+                hash += 3;
+        }
+        for(int i = 9; i < s.size(); i++){
+            hash = hash << 2;
+            c = s[i];
+            if(c == 'C')
+                hash += 1;
+            else if(c == 'G')
+                hash += 2;
+            else if(c == 'T')
+                hash += 3;
+            //remove the higher(more than 20) bits
+            hash = hash & 0xfffff;
+            if(hashTable[hash]++ == 1){
+                ret.push_back(s.substr(i - 9, 10));
             }
-            it = m.find(hash);
-            if(it == m.end())
-                m[hash] = 1;
-            else if(it->second == 1){
-                ret.push_back(subString);
-                it->second++;
-            }
-        }   
+        }
         return ret;
     }
 };
