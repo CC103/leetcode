14c69a151306fe4f6ca3dbc690f1227f939424db
Chuck Liu <yimingl816@gmail.com>
2016-08-31 17:31:42 +0800

    Update 011. Container With Most Water.cpp
    Solution 2: two pointers; fast

--- cpp/011. Container With Most Water.cpp
+++ cpp/011. Container With Most Water.cpp
@@ -2,21 +2,15 @@ class Solution {
 public:
     int maxArea(vector<int>& height) {
         int max = 0;
-        int volume;
-        int size = height.size();
-        int leftHighest = height[0];
-        int rightHighest;
-        for(int i = 0; i < size; i++){
-            if(height[i] < leftHighest)
-                continue;
-            leftHighest = height[i];
-            rightHighest = height[size - 1];
-            for(int j = size - 1; j > i; j--){
-                if(height[j] < rightHighest)
-                    continue;
-                rightHighest = height[j];
-                volume = (j - i) * min(height[i], height[j]);
-                max = volume > max ? volume : max;
+        int low = 0;
+        int high = height.size() - 1;
+        while(low < high){
+            int volume = (high - low) * min(height[low], height[high]);
+            max = volume > max ? volume : max;
+            if(height[low] < height[high])
+                low++;
+            else{
+                high--;
             }
         }
         return max;
