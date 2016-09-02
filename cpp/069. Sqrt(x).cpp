753cf9366d91175eca22775b1532935e36ec45a8
Chuck Liu <yimingl816@gmail.com>
2016-09-02 16:48:05 +0800

    Update and rename 69. Sqrt(x).cpp to 069. Sqrt(x).cpp
    Solution 2: binary search with some optimization

--- cpp/069. Sqrt(x).cpp
+++ cpp/069. Sqrt(x).cpp
@@ -2,14 +2,14 @@ class Solution {
 public:
     int mySqrt(int x) {
         int start = 0;
-        int end = x;
+        int end = x < INT_MAX ? x : INT_MAX;
         while(start < end){
             int mid = (start + end + 1) / 2;
-            cout << start << " " << mid << " " << end <<endl;
-            long int mul = mid * mid;
-            if(mul == x)
+            //cout << start << " " << mid << " " << end <<endl;
+            
+            if(mid == x / mid)
                 return mid;
-            if(mul > x){
+            if(mid > x / mid){
                 end = mid - 1;
             }
             else
