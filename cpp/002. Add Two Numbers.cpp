cb47aca7e1673d36199610cc4df246e27c697eb7
Chuck Liu <yimingl816@gmail.com>
2016-09-12 11:01:46 +0800

    Update 002. Add Two Numbers.cpp
    Solution 2: very concise solution

--- cpp/002. Add Two Numbers.cpp
+++ cpp/002. Add Two Numbers.cpp
@@ -9,79 +9,18 @@
 class Solution {
 public:
     ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
-        if(l1 == NULL)
-            return l2;
-        if(l2 == NULL)
-            return l1;
-        ListNode* head = l1;
-        int sum = 0, carry = 0;
-        //cout << 1;
-        while(l1->next != NULL && l2->next != NULL){
-            sum = l1->val + l2->val + carry;
-            l1->val = sum % 10;
+        int carry = 0;
+        ListNode preHead(0);
+        ListNode *p = &preHead;
+        int sum;
+        while(l1 || l2 || carry){
+            sum = (l1 ? l1->val : 0) + (l2 ? l2->val : 0) + carry;
             carry = sum / 10;
-            l1 = l1->next;
-            l2 = l2->next;
+            p->next = new ListNode(sum % 10);
+            p = p->next;
+            l1 = l1 ? l1->next : l1;
+            l2 = l2 ? l2->next : l2;
         }
-        //cout << 2;
-        // calculate the last common digit
-        sum = l1->val + l2->val + carry;
-        l1->val = sum % 10;
-        carry = sum / 10;
-        // the length of two lists are same
-        // cout << l1->next << " " << l2->next;
-        if(l1->next == NULL && l2->next == NULL){
-            cout << 1 << " " << carry;
-            if(carry){
-                cout << 2;
-                ListNode newNode(1);
-                cout << 3;
-                l1->next = &newNode;
-                cout << 4;
-            }
-            cout << 5;
-            return head;
-        }
-        // l1 is shorter than l2
-        if(l1->next == NULL){
-            l1->next = l2->next;
-            l1 = l1->next;
-            while(carry && l1->next != NULL){
-                sum = l1->val + carry;
-                l1->val = sum % 10;
-                carry = sum / 10;
-                l1 = l1->next;
-            }
-            if(carry && l1->next == NULL){
-                sum = l1->val + carry;
-                l1->val = sum % 10;
-                carry = sum / 10;
-                if(carry){
-                    ListNode newNode(1);
-                    l1->next = &newNode;
-                }
-            }
-            return head;
-        }
-        // l2 is shorter than l1
-        if(l2->next == NULL){
-            l1 = l1->next;
-            while(carry && l1->next != NULL){
-                sum = l1->val + carry;
-                l1->val = sum % 10;
-                carry = sum / 10;
-                l1 = l1->next;
-            }
-            if(carry && l1->next == NULL){
-                sum = l1->val + carry;
-                l1->val = sum % 10;
-                carry = sum / 10;
-                if(carry){
-                    ListNode newNode(1);
-                    l1->next = &newNode;
-                }
-            }
-        }
-        return head;
+        return preHead.next;
     }
 };
