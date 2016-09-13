005a1d2f621e11115839279b29a03205e9bb19e4
Chuck Liu <yimingl816@gmail.com>
2016-09-13 15:54:45 +0800

    Update 322. Coin Change.cpp
    Solution 2: dp, bottom up

--- cpp/322. Coin Change.cpp
+++ cpp/322. Coin Change.cpp
@@ -4,24 +4,20 @@ public:
         if(amount == 0 || coins.empty())
             return 0;
         sort(coins.begin(), coins.end());
-        int min = INT_MAX;
-        dp(coins, amount, 0, min);
-        return min == INT_MAX ? -1 : min;
-    }
-    void dp(vector<int> &coins, int amount, int num, int &min){
-        //cout << "amount: " << amount << " " << "num: " << num << endl;
-        if(amount < coins[0])
-            return;
-        for(auto i : coins){
-            if(amount == i){
-                min = min < num + 1 ? min : num + 1;
-                return;
+        // there is potential risk here. If max = INT_MAX, there will be overflow 
+        int max = amount + 1;
+        vector<int> dp(amount + 1, max);
+        dp[0] = 0;
+        for(int i = 1; i <= amount; i++){
+            for(int j = 0; j < coins.size(); j++){
+                if(i >= coins[j]){
+                    dp[i] = dp[i] < dp[i - coins[j]] + 1 ? dp[i] : dp[i - coins[j]] + 1;
+                }
+                else
+                    break;
             }
-            if(amount > i){
-                dp(coins, amount - i, num + 1, min);
-            }
-            if(amount < i)
-                return;
         }
+        return dp[amount] > amount ? -1 : dp[amount];
+        
     }
 };
