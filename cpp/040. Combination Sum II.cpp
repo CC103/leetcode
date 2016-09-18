ca47028298dd5ca42861ab9c7723e27ee47c84cf
Chuck Liu <yimingl816@gmail.com>
2016-09-18 17:06:25 +0800

    Update 040. Combination Sum II.cpp
    Solution 1: backtracking with iterative method

--- cpp/040. Combination Sum II.cpp
+++ cpp/040. Combination Sum II.cpp
@@ -1,39 +1,32 @@
 class Solution {
 public:
     vector<vector<int>> combinationSum2(vector<int>& candidates, int target) {
-        vector<vector<int>> res;
-        vector<int> cur;
-        map<int, int> m;
+        vector<vector<int>> ret;
+        vector<int> instance;
         sort(candidates.begin(), candidates.end());
-        for(int i : candidates){
-            if(m.find(i) == m.end())
-                m[i] = 1;
-            else
-                m[i]++;
-        }
-        int sum = 0;
-        backtrack(res, cur, m, sum, target);
-        return res;
+        backtrack(ret, instance, target, 0, candidates);
+        return ret;
     }
-    void backtrack(vector<vector<int>> &res, vector<int> &cur, map<int, int> &m, int &sum, int target){
-        if(!cur.empty()){
-            sum += cur.back();
-            if(sum == target){
-                res.push_back(cur);
-                return;
-            }
-            if(sum > target)
-                return;
+    void backtrack(vector<vector<int>> &ret, vector<int> &instance, int target, int start, vector<int> &candidates){
+        if(target == 0){
+            ret.push_back(instance);
+            return;
         }
-        for(auto & i : m){
-            if(i.second <= 0)
-                continue;
-            cur.push_back(i.first);
-            i.second--;
-            backtrack(res, cur, m, sum, target);
-            sum -= cur.back();
-            cur.pop_back();
-            i.second++;
+        else{
+            for(int i = start; i < candidates.size(); i++){
+                if(candidates[i] > target){
+                    return;
+                }
+                else{
+                    instance.push_back(candidates[i]);
+                    backtrack(ret, instance, target - candidates[i], i + 1, candidates);
+                    instance.pop_back();
+                    while(i + 1 < candidates.size() && candidates[i + 1] == candidates[i]){
+                        i++;
+                    }
+                }
+            }
+
         }
     }
 };
