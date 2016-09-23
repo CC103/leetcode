1433cd66936035ea3a762217545a07b4bab5de31
Chuck Liu <yimingl816@gmail.com>
2016-09-23 17:20:12 +0800

    Update 390. Elimination Game.cpp
    Solution 2

--- cpp/390. Elimination Game.cpp
+++ cpp/390. Elimination Game.cpp
@@ -1,38 +1,31 @@
 class Solution {
 public:
     int lastRemaining(int n) {
-        int *num = new int[n];
-        for(int i = 0; i < n; i++){
-            num[i] = i + 1;
+        return leftToRight(n);
+
+    }
+    int leftToRight(int n){
+        if(n <= 2){
+            return n;
         }
-        while(n > 1){
-            int size = n;
-            n /= 2;
-            int *leftToRight = new int[n];
-            for(int i = 1, j = 0; i < size; i += 2, j++){
-                leftToRight[j] = num[i];
-            }
-            delete num;
-            if(n == 1){
-                return leftToRight[0];
+        else{
+            return 2 * rightToRight(n / 2);
+        }
+    }
+    int rightToRight(int n){
+        if(n <= 2){
+            return 1;
+        }
+        else{
+            if(n % 2 == 1){
+                return 2 * leftToRight(n / 2);
             }
             else{
-                int size = n;
-                n /= 2;
-                int *rightToleft = new int[n];
-                for(int i = size - 2, j = n - 1; i >= 0; i -= 2, j--){
-                    rightToleft[j] = leftToRight[i];
-                }
-                delete leftToRight;
-                if(n == 1){
-                return rightToleft[0];
-                }
-                else{
-                    num = rightToleft;
-                }
+                // 1, 3 (+1) -> 2, 4 (/2) -> 1, 2
+                // we need  multiple 2 first and then minus 1 to get true value 
+                return 2 * leftToRight(n / 2) - 1;
             }
         }
-        // initial n = 1
-        return num[0];
+        
     }
 };
