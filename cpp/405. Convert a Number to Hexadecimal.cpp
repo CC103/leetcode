8b37ac723d0c05f69f38dddd5b1ba0012e85b3f1
Chuck Liu <yimingl816@gmail.com>
2016-09-27 17:07:57 +0800

    Update 405. Convert a Number to Hexadecimal.cpp
    Solution 1: much more concise

--- cpp/405. Convert a Number to Hexadecimal.cpp
+++ cpp/405. Convert a Number to Hexadecimal.cpp
@@ -2,7 +2,6 @@ class Solution {
 public:
     string toHex(int num) {
         unsigned n = unsigned(num);
-        cout << n << endl;
         int hex[8];
         for(int i = 0; i < 8; i++){
             // 00..001111
@@ -23,20 +22,7 @@ public:
                 ret.push_back(hex[i] + '0');
             }
             else{
-                if(hex[i] == 10)
-                    ret.push_back('a');
-                else if(hex[i] == 11)
-                    ret.push_back('b');
-                else if(hex[i] == 12)
-                    ret.push_back('c');
-                else if(hex[i] == 13)
-                    ret.push_back('d');
-                else if(hex[i] == 14)
-                    ret.push_back('e');
-                else if(hex[i] == 15)
-                    ret.push_back('f');
-                else
-                    cout << "error hex number" << endl;
+                ret.push_back(hex[i] - 10 + 'a');
             }
         }
         if(ret.empty())
