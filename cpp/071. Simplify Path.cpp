a43a86b5c1418fd69ecaa0a20e53208eec041381
Chuck Liu <yimingl816@gmail.com>
2016-09-28 14:35:15 +0800

    Update 071. Simplify Path.cpp
    Solution 2: much more concise code using getline

--- cpp/071. Simplify Path.cpp
+++ cpp/071. Simplify Path.cpp
@@ -1,47 +1,15 @@
 class Solution {
 public:
     string simplifyPath(string path) {
-        stack<string> strStack;
-        string str;
-        for(int i = 0; i < path.size(); i++){
-            if(path[i] == '/'){
-                if(!str.empty() && str != "."){
-                    strStack.push(str);
-                }
-                str.clear();
-            }
-            else{
-                str.push_back(path[i]);
-            }
+        string res, tmp;
+        vector<string> stk;
+        stringstream ss(path);
+        while(getline(ss,tmp,'/')) {
+            if (tmp == "" or tmp == ".") continue;
+            if (tmp == ".." and !stk.empty()) stk.pop_back();
+            else if (tmp != "..") stk.push_back(tmp);
         }
-        // the path may not end with '/'
-        if(!str.empty() && str != ".")
-            strStack.push(str);
-        stack<string> simplifyStrStack;
-        int numOfReturn = 0;
-        while(!strStack.empty()){
-            string temp = strStack.top();
-            if(temp == ".."){
-                numOfReturn += 1;
-                strStack.pop();
-            }
-            else{
-                if(numOfReturn){
-                    numOfReturn -= 1;
-                    strStack.pop();
-                }
-                else{
-                    simplifyStrStack.push(temp);
-                    strStack.pop();
-                }
-            }
-        }
-        string ret;
-        while(!simplifyStrStack.empty()){
-            ret = ret + "/" + simplifyStrStack.top();
-            simplifyStrStack.pop();
-        }
-        if(ret.empty()) return "/";
-        return ret;
+        for(auto str : stk) res += "/"+str;
+        return res.empty() ? "/" : res;
     }
 };
