fce0431dcec0ef6b0122857bd6fa14487a0aaa66
Chuck Liu <yimingl816@gmail.com>
2016-10-28 19:34:47 +0800

    Create 287. Find the Duplicate Number.cpp
    Solution 1: Floyd Cycle Detection Algorithm

---
+++ cpp/287. Find the Duplicate Number.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int findDuplicate(vector<int>& nums) {
+        if(nums.size() < 2) return -1;
+        int fast = nums[nums[0]];
+        int slow = nums[0];
+        while(fast != slow){
+            fast = nums[nums[fast]];
+            slow = nums[slow];
+        }
+        fast = 0;
+        while(fast != slow){
+            fast = nums[fast];
+            slow = nums[slow];
+        }
+        return slow;
+    }
+};
