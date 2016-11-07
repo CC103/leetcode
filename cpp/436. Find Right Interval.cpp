5ad7a1f2d100b8e1a04bbba00fdc260cc382a45a
Chuck Liu <yimingl816@gmail.com>
2016-11-07 17:45:18 +0800

    Update 436. Find Right Interval.cpp
    Solution 1

--- cpp/436. Find Right Interval.cpp
+++ cpp/436. Find Right Interval.cpp
@@ -1,1 +1,25 @@
-
+/**
+ * Definition for an interval.
+ * struct Interval {
+ *     int start;
+ *     int end;
+ *     Interval() : start(0), end(0) {}
+ *     Interval(int s, int e) : start(s), end(e) {}
+ * };
+ */
+class Solution {
+public:
+    vector<int> findRightInterval(vector<Interval>& intervals) {
+        map<int, int> hash;
+        for(int i = 0; i < intervals.size(); i++){
+            hash[intervals[i].start] = i; 
+        }
+        std::vector<int> ret;
+        for(auto it : intervals){
+            auto right = hash.lower_bound(it.end);
+            if(right == hash.end()) ret.push_back(-1);
+            else ret.push_back(right->second);
+        }
+        return ret;
+    }
+};
