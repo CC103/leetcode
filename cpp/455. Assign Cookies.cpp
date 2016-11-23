5650c287133e65a3b86d067cb0329dc3290ea1f7
Chuck Liu <yimingl816@gmail.com>
2016-11-23 21:43:58 +0800

    Update 455. Assign Cookies.cpp
    Solution 1: greedy

--- cpp/455. Assign Cookies.cpp
+++ cpp/455. Assign Cookies.cpp
@@ -1,1 +1,21 @@
-
+class Solution {
+public:
+    int findContentChildren(vector<int>& g, vector<int>& s) {
+        if(g.empty() || s.empty()) return 0;
+        sort(g.begin(), g.end());
+        sort(s.begin(), s.end());
+        int num = 0;
+        int i = 0, j = 0;
+        while(i < s.size() && j < g.size()){
+            if(s[i] >= g[j]){
+                num++;
+                i++;
+                j++;
+            }
+            else{
+                i++;
+            }
+        }
+        return num;
+    }
+};
