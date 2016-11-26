7affb90b2e2cd2d8485650162c0169953d63e1d0
Chuck Liu <yimingl816@gmail.com>
2016-11-26 21:08:20 +0800

    Solution 2：optimized algorithm, faster

--- tag/Array/219. Contains Duplicate II.cpp
+++ tag/Array/219. Contains Duplicate II.cpp
@@ -1,19 +1,16 @@
 class Solution {
 public:
     bool containsNearbyDuplicate(vector<int>& nums, int k) {
-        unordered_map<int, vector<int>> hash;
-        unordered_map<int, vector<int>>::iterator it;
+        unordered_map<int, int> hash;
+        unordered_map<int, int>::iterator it;
         for(int i = 0; i < nums.size(); i++){
         	it = hash.find(nums[i]);
         	if(it == hash.end()){
-        		vector<int> v(1, i);
-        		hash[nums[i]] = v;
+        		hash[nums[i]] = i;
         	}
         	else{
-        		for(int j = 0; j < it->second.size(); j++){
-        			if(i - it->second[j] <= k) return true;
-        		}
-        		it->second.push_back(i);
+        		if(i - it->second <= k) return true;
+        		else it->second = i;
         	}
         }
         return false;
