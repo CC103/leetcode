00d01de826b77e2614c3300db9f682140dec5f98
Chuck Liu <yimingl816@gmail.com>
2016-11-28 23:05:14 +0800

    Solution 1

---
+++ tag/Array/066. Plus One.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    vector<int> plusOne(vector<int>& digits) {
+        if(digits.empty()){
+        	digits.insert(digits.begin(), 1);
+        	return digits;
+        }
+        int carry = 1, sum;
+        for(int i = digits.size() - 1; i >= 0; i--){
+        	sum = digits[i] + carry;
+        	digits[i] = sum % 10;
+        	carry = sum / 10;
+        }
+        if(carry) digits.insert(digits.begin(), 1);
+        return digits;
+    }
+};
