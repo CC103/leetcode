ac5ca77082b3a4275059ba785bc069b0c17fb1f9
Chuck Liu <yimingl816@gmail.com>
2016-11-28 22:44:09 +0800

    faster

--- tag/Array/283. Move Zeroes.cpp
+++ tag/Array/283. Move Zeroes.cpp
@@ -3,18 +3,19 @@ public:
     void moveZeroes(vector<int>& nums) {
         if(nums.size() < 2) return;
         int fir = 0;
+        while(nums[fir] != 0 && fir < nums.size()) fir++;
+        int sec = fir;
         while(fir < nums.size()){
-        	if(nums[fir] != 0) fir++;
-        	else{
-        		int sec = fir;
-        		while(nums[sec] == 0){
-        			sec++;
-        			// the nail is full of zero
-        			if(sec == nums.size()) return;
-        		}
-        		swap(nums[fir], nums[sec]);
-        		fir++;
-        	}
+            if(nums[fir] != 0) fir++;
+            else{
+                while(nums[sec] == 0){
+                    sec++;
+                    // the nail is full of zero
+                    if(sec == nums.size()) return;
+                }
+                swap(nums[fir], nums[sec]);
+                fir++;
+            }
         }
     }
 };
