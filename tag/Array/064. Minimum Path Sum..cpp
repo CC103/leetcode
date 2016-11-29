52d054902472a82f6cb80856896d228fa696582f
Chuck Liu <yimingl816@gmail.com>
2016-11-29 21:29:42 +0800

    Solution 1：dp

--- tag/Array/064. Minimum Path Sum..cpp
+++ tag/Array/064. Minimum Path Sum..cpp
@@ -1,26 +1,22 @@
 class Solution {
 public:
     int minPathSum(vector<vector<int>>& grid) {
-        if(grid.empty()) return 0;
-        return dp(grid, 0, 0);
-    }
-    int dp(vector<vector<int>>& grid, int m, int n){
-    	if(m == grid.size() - 1){
-    		int sum = 0;
-    		for(; n < grid[0].size(); n++){
-    			sum += grid[m][n];
-    		}
-    		return sum;
+    	if(grid.empty()) return 0;
+    	int m = grid.size(), n = grid[0].size();
+    	vector<vector<int>> sum(m, vector<int>(n, 0));
+    	sum[0][0] = grid[0][0];
+    	for(int i = 1; i < m; i++){
+    		sum[i][0] = sum[i - 1][0] + grid[i][0]; 
     	}
-    	else if(n == grid[0].size() - 1){
-    		int sum = 0;
-    		for(; m < grid.size(); m++){
-    			sum += grid[m][n];
-    		}
-    		return sum;
+    	for(int i = 1; i < n; i++){
+    		sum[0][i] = sum[0][i - 1] + grid[0][i]; 
     	}
-    	else{
-    		return min(dp(grid, m + 1, n), dp(grid, m, n + 1)) + grid[m][n];
-    	}		
+    	for(int i = 1; i < m; i++){
+    		for(int j = 1; j < n; j++){
+    			sum[i][j] = grid[i][j] + min(sum[i - 1][j], sum[i] [j - 1]);
+    		}
+    	}
+    	return sum[m - 1][n - 1];
+
     }
 };
