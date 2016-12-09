85e8df226c71df96c3f967503c3821b6c7fe72dd
Chuck Liu <yimingl816@gmail.com>
2016-12-09 16:49:00 +0800

    Solution 2：Binary Search

--- tag/Array/074. Search a 2D Matrix.cpp
+++ tag/Array/074. Search a 2D Matrix.cpp
@@ -2,35 +2,19 @@ class Solution {
 public:
     bool searchMatrix(vector<vector<int>>& matrix, int target) {
     	if(matrix.empty()) return false;
-        int len = matrix[0].size();
-        int start = 0, end = matrix.size() - 1, mid;
-        while (start <= end) {
-        	mid = (start + end) / 2;
-        	if (target >= matrix[mid][0] && target <= matrix[mid][len - 1]) {
-        		int fir = 0, thir = len - 1, sec;
-        		while (fir <= thir) {
-        			sec = (fir + thir) / 2;
-        			if (target == matrix[mid][sec]) return true;
-        			else{
-        				if (target > matrix[mid][sec]) {
-        					fir = sec + 1;
-        				}
-        				else {
-        					thir = sec - 1;
-        				}
-        			}
-        		}
-        		return false;
-        	}
-        	else {
-        		if (target > matrix[mid][len - 1]) {
-        			start = mid + 1;
-        		}
-        		else if (target < matrix[mid][0]) {
-        			end = mid - 1;
-        		}
-        	}
-        }
-        return false;
+    	int row = matrix.size();
+    	int col = matrix[0].size();
+    	int start = 0, end = row * col - 1, mid;
+    	while (start <= end) {
+    		mid = (start + end) / 2;
+    		if (matrix[mid / col][mid % col] == target) return true;
+    		else if (matrix[mid / col][mid % col] > target) {
+    			end = mid - 1;
+    		}
+    		else {
+    			start = mid + 1;
+    		}
+    	}
+    	return false;
     }
 };
