3a89971604e2755766e9a66f30bca4dbc6b5f236
Chuck Liu <yimingl816@gmail.com>
2016-12-09 10:26:46 +0800

    Solution 2：much more concise with two pointers

--- tag/Array/080. Remove Duplicates from Sorted Array II.cpp
+++ tag/Array/080. Remove Duplicates from Sorted Array II.cpp
@@ -5,32 +5,28 @@ public:
         int fir = 0, sec = 1, next;
         bool isDuplicated = false;
         for(; sec < nums.size();){
+        	next = fir + 1;
         	if(nums[sec] != nums[fir]){
-        		next = fir + 1;
-        		if(sec != next){
-        			nums[next] = nums[sec];
-        			fir++;
+        		if(sec == next){
+        			fir = next;
         			sec++;
-        			isDuplicated = false;
         		}
         		else{
-        			fir++;
+        			nums[next] = nums[sec];
+        			fir = next;
         			sec++;
-        			isDuplicated = false;
         		}
+        		isDuplicated = false;
         	}
         	else{
         		if(!isDuplicated){
-        			// make the second same element
-        			nums[fir + 1] = nums[sec];
+        			nums[next] = nums[sec];
         			fir++;
-        			sec++;
         			isDuplicated = true;
         		}
-        		else{		
+        		else{
         			sec++;
         		}
-        		
         	}
         }
         return fir + 1;
